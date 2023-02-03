bb7acd3cb92beb9bfbd0772b739287f8a45b8c0b
Fernando <fernandosuarez1985@gmail.com>
2023-02-03 11:26:26 -0300

    mensaje

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
-const nombre = prompt ("ingrese su nombre de usuario");
-const password = parseInt(prompt("Ingrese una clave de 4 numeros"));
-alert("Gracias por ingresar, ahora vamos a registrarnos");
-let user = prompt("Ingrese su Nombre de Usuario");
-let clave = parseInt(prompt("Ingrese su Contraseña"));
 
-for (let i=0; i<5; i= +i){
-    if (nombre == user & password == clave){
-        alert("Usuario y Contraseña son correctos");
-        break;
-    } else{
-        alert("Usuario y Contraseña son incorrectos, ingresar de nuevo");
-        user = prompt("Ingrese su Nombre de Usuario");
-        clave = prompt("Ingrese su Contraseña");
-        break;
-    } 
+let nombre = prompt("Ingrese su usuario");
+let password = parseInt(prompt("Ingrese una clave de 4 numeros"));
 
+while (password.toString().length < 4){
+    alert("Usuario y Contraseña son incorrectos, ingresar de nuevo");
+    password = prompt("Ingrese una clave de 4 numeros");
 }
-
 
 let zapatillas;
 let operacion;
@@ -25,26 +14,26 @@ let contador = 0;
 
 function eleccion() {
     //alert('Elija una zapatilla');
-    zapatillas = prompt('Elija un tipo de zapatillas de acuerdo si es Hombre, Mujer, Niño').toLowerCase();
+    zapatillas = prompt('Que tipo de zapatillas desea comprar: 1.hombre, 2.mujer o 3.niño').toLowerCase();
     nuevaZapatillas(zapatillas);
 }
 
 
 eleccion();
-nuevaZapatillas(zapatillas)
+nuevaZapatillas(zapatillas);
 
 function nuevaZapatillas(zapatillas){
     switch (zapatillas) {
         case 'hombre':
-            /* alert("elija que zapatillas desea: Nike, Asics, New Balance"); */
-            let zapatillasHombre = prompt ('Nike, Asics, New Balance').toLowerCase();
-            if (zapatillasHombre === 'Nike') {
+            /* alert(`elija que zapatillas desea a, b, c `);*/   
+            let zapatillasHombre = prompt('1.Nike 2.Asics 3.New Balance').toLowerCase();
+            if (zapatillasHombre === '1') {
                 zapatillasHombre = 150;
                 precio(zapatillasHombre);
-            } else if (zapatillasHombre === 'Asics') {
+            } else if (zapatillasHombre === '2') {
                 zapatillasHombre = 200;
                 precio(zapatillasHombre);
-            } else if (zapatillasHombre === 'New Balance') {
+            } else if (zapatillasHombre === '3') {
                 zapatillasHombre = 250;
                 precio(zapatillasHombre);
             } else{
@@ -53,15 +42,15 @@ function nuevaZapatillas(zapatillas){
             break;
 
         case 'mujer':
-            /* alert("elija que zapatillas desea: Adidas, Puma, Topper").toLowerCase(); */
-            let zapatillasMujer = prompt ('Adidas, Puma, Topper').toLowerCase();
-            if (zapatillasMujer === 'Adidas') {
+            /* alert("elija que zapatillas desea: a, b, c");  */
+            let zapatillasMujer = prompt ('1. Adidas 2.Puma 3.Topper').toLowerCase();
+            if (zapatillasMujer === '1') {
                 zapatillasMujer = 120;
                 precio(zapatillasMujer);
-            } else if (zapatillasMujer === 'Puma') {
+            } else if (zapatillasMujer === '2') {
                 zapatillasMujer = 150;
                 precio(zapatillasMujer);
-            } else if (zapatillasMujer === 'Topper') {
+            } else if (zapatillasMujer === '3') {
                 zapatillasMujer = 180;
                 precio(zapatillasMujer);
             } else{
@@ -70,15 +59,15 @@ function nuevaZapatillas(zapatillas){
             break;
 
         case 'niño':
-            /* alert("elija que zapatillas desea: Fila, Saucony, Reebok").toLowerCase(); */
-            let zapatillasNinio = prompt ('Fila').toLowerCase();
-            if (zapatillasNinio === 'Fila, Saucony, Reebok') {
+            alert("elija que zapatillas desea: a, b, c"); 
+            let zapatillasNinio = prompt ('1.Fila 2.Saucony 3.Reebok').toLowerCase();
+            if (zapatillasNinio === '1') {
                 zapatillasNinio = 30;
                 precio(zapatillasNinio);
-            } else if (zapatillasNinio === 'Saucony') {
+            } else if (zapatillasNinio === '2') {
                 zapatillasNinio = 90;
                 precio(zapatillasNinio);
-            } else if (zapatillasNinio === 'Reebok') {
+            } else if (zapatillasNinio === '3') {
                 zapatillasNinio = 60;
                 precio(zapatillasNinio);
             } else {
@@ -107,6 +96,7 @@ function agregarItem(item){
         eleccion();
     } else if (confirmacion === false){
         alert ('Gracias, vuelva pronto');
+        Esc;
     } 
 }
 
@@ -115,3 +105,29 @@ function suma(item){
 }
 
 nuevaZapatillas(zapatillas);
+
+/* cuotasZapatillas = parseInt(prompt("que cantidad de cuotas 1, 3, 6, 12"));
+while (cuotasZapatillas!= 1 && cuotasZapatillas!= 3 && cuotasZapatillas!= 6 && cuotasZapatillas!= 12 ){
+    alert("No se puede elegir estas cuotas")
+    cuotasZapatillas = parseInt(prompt("Que opcion desea = 1, 3, 6, 12"));
+}
+
+if (zapatillas == Nike) {
+    switch (cuotasZapatillas){
+        case 1:
+            alert("El precio de las zapatillas es " + Nike);
+            break;
+        case 3:
+            alert("El precio de las zapatillas es " + (Nike + ((Nike*20)/100)));
+            alert("El precio de las zapatillas es " + (Nike + ((Nike*20)/100))/3);
+            break;
+        case 3:
+            alert("El precio de las zapatillas es " + (Nike + ((Nike*40)/100)));
+            alert("El precio de las zapatillas es " + (Nike + ((Nike*40)/100))/6);
+            break;
+        case 3:
+            alert("El precio de las zapatillas es " + (Nike + ((Nike*60)/100)));
+            alert("El precio de las zapatillas es " + (Nike + ((Nike*60)/100))/12);
+            break;
+    }
+} */
